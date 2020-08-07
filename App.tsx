2572adcb7f22626e10a15507328c7a0523bcec08
Thau26 <thauanyaraujoalves@hotmail.com>
2020-08-07 14:23:14 -0300

    inicialização do projeto

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello wORLS</Text>
+      <Text>Heedskfiud</Text>
       <StatusBar style="auto" />
     </View>
   );
